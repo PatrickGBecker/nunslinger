64c91740f13a136131c3ad519045fdab34a7cc48
emmacintosh <emmammrussell@gmail.com>
2022-09-14 14:17:59 -0600

    Add first cutscene dialogue

--- src/component/Missions/MissionCutscenes.tsx.tsx
+++ src/component/Missions/MissionCutscenes.tsx.tsx
@@ -2,19 +2,25 @@ import React from 'react';
 
 const MissionCutscenes = {
     mission1:  [
-        `Sister Beretta:`,
-        `Father Felix:`
+        `Sister Beretta: Forgive me father, for I have sinned. My last confession was-`,
+        `Father Felix: I know when it was, where the hell have you been for the last two years?`,
+        `Sister Beretta: Recovering.`,
+        `Father Felix: Oh, right. The Archbishop… my apologies, we… never really put together what happened there. Not that there was much of him left to put back together.`,
+        `Sister Beretta: Never shoot a possessed man in the liver. Never.`,
+        `Father Felix: Noted. Am I correct in assuming you're here about the priest in Vienna?`,
+        `Sister Beretta: Yes.`,
+        `Father Felix: Right, well… are you able to offer us your assistance?`,
     ],
     mission2: [
-        `Sister Beretta:`,
+        `Sister Beretta: Forgive me father, for I have sinned.`,
         `Father Felix:`
     ],
     mission3: [
-        `Sister Beretta:`,
+        `Sister Beretta: Forgive me father, for I have sinned.`,
         `Father Felix:`
     ],
     mission4: [
-        `Sister Beretta:`,
+        `Sister Beretta: Forgive me father, for I have sinned.`,
         `Father Felix:`
     ]
 }
