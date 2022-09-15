475c43feee74b2fac9389426c74c10afd10d5ec5
emmacintosh <emmammrussell@gmail.com>
2022-09-15 17:13:39 -0600

    Add instruction rendering logic, add Patrick's celebration bugfix

--- src/component/Game/Game.tsx
+++ src/component/Game/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import Player from '../Characters/Player';
@@ -9,9 +9,49 @@ import './Game.css';
 // import HealthBar from '../HealthBar/HealthBar'
 const music = require('../../Assets/Music/testMusic.mp3');
 
-const Game = () => {
+interface Iprops{
+    celebration: string
+}
 
-const [celebration, setCelebration] = useState<string>('')
+const Game = (props: Iprops) => {
+
+const [instructions, setInstructions] = useState<any>(false)
+const [missionCount, setmissionCount] = useState<number>(0)
+const [gameCount, setGameCount] = useState<number>(0)
+const [shotFirst, setShotFirst] = useState<boolean>(false)
+let gameInstructions:any
+
+// const setTimeout(() => {
+    
+// }, timeout);
+
+
+const hideInstructions = () => {
+    setInstructions(false)
+}
+
+useEffect(() => {
+    if (gameCount === 0){
+        setInstructions(true)
+    }
+}, [])
+
+useEffect(() => {
+    
+    if (gameCount === 0 || instructions === true) {
+        gameInstructions = () => {return(
+            <div className="game-instructions">
+                instructions go here
+            </div>
+        )}
+    } else {
+        gameInstructions = ''
+    }
+    return gameInstructions
+}, [gameCount, instructions])
+
+//save it as a variable that only renders the div if both conditions are met, put the instructions in the useeffect div
+
 
     return (
         <div className="game-background">
@@ -46,6 +86,8 @@ const [celebration, setCelebration] = useState<string>('')
                     <Link to='/'>
                         <button className='back-to-main-page'>Retire from Hunting?</button>
                     </Link>
+                    {gameInstructions}
+                    <button className='dismiss-instructions-button' onClick={hideInstructions}>Less reading, more shooting</button>
                 </div>
             </section>
         </div>
