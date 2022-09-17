d52d3fdd777508f8a7faae8705b74e629d5460ac
Cleveland Ticoalu <ayocleveland@gmail.com>
2022-09-16 20:12:01 -0600

    setup trigger button

---
+++ src/component/Characters/character.ts
@@ -0,0 +1,12 @@
+
+
+class Character {
+    minFireTime: number
+    maxFireTime: number
+    constructor(minFireTime: number, maxFireTime: number) {
+        this.minFireTime = minFireTime
+        this.maxFireTime = maxFireTime
+    }
+}
+
+export default Character;
